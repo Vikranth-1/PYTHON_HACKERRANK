91f6aaa0f2addb06feb8f587a626ca5ad305dd37
Vikranth V <vvikranth08@gmail.com>
2026-05-11 07:09:59 +0530

    Create Matrix Script.py

---
+++ Matrix Script.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+m = int(first_multiple_input[1])
+
+matrix = []
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+
+decoded = ''.join(matrix[j][i] for i in range(m) for j in range(n))
+
+print(re.sub(r'(?<=[A-Za-z0-9])([^A-Za-z0-9]+)(?=[A-Za-z0-9])', ' ', decoded))
