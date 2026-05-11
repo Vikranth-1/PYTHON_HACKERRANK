02bb1e49beb25aca6d0f26ec9683dd97cfdcb9ba
Vikranth V <vvikranth08@gmail.com>
2026-05-11 07:12:29 +0530

    Create Designer Door Mat.py

---
+++ Designer Door Mat.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+pattern = '.|.'
+for i in range(n // 2):
+    print((pattern * (2 * i + 1)).center(m, '-'))
+
+print('WELCOME'.center(m, '-'))
+
+for i in range(n // 2 - 1, -1, -1):
+    print((pattern * (2 * i + 1)).center(m, '-'))
