e6e382caf55a7397bc258066c1490dc9ae5d351e
Vikranth V <vvikranth08@gmail.com>
2026-05-13 14:19:27 +0530

    Create Validating phone numbers.py

---
+++ Validating phone numbers.py
@@ -0,0 +1,10 @@
+import re
+
+if __name__ == "__main__":
+    n = int(input())
+    for _ in range(n):
+        number = input().strip()
+        if re.match(r'^[789]\d{9}$', number):
+            print("YES")
+        else:
+            print("NO")
