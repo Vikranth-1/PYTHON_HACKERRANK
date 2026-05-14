fa179dac2975bb52cb4155c55c993129e8aaeec7
Vikranth V <vvikranth08@gmail.com>
2026-05-14 07:16:41 +0530

    Create Company Logo.py

---
+++ Company Logo.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+if __name__ == '__main__':
+    s = input()
+    
+    counts = Counter(s)
+    
+    sorted_chars = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+    
+    for i in range(3):
+        print(f"{sorted_chars[i][0]} {sorted_chars[i][1]}")
