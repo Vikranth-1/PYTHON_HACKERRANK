32a45ac5dca2fb9e2026a75b0e4325beaccd8be1
Vikranth V <vvikranth08@gmail.com>
2026-05-16 13:58:30 +0530

    Create itertools.product().py

---
+++ itertools.product().py
@@ -0,0 +1,8 @@
+from itertools import product
+
+if __name__ == "__main__":
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    
+    result = list(product(A, B))
+    print(' '.join(str(tuple(x)) for x in result))
