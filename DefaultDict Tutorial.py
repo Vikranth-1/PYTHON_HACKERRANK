9db9aef2f189e84503b3b72034b137225036b2c1
Vikranth V <vvikranth08@gmail.com>
2026-05-18 00:25:38 +0530

    Create DefaultDict Tutorial.py

---
+++ DefaultDict Tutorial.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    
+    d = defaultdict(list)
+    
+    for i in range(1, n + 1):
+        word = input()
+        d[word].append(i)
+    
+    for _ in range(m):
+        word = input()
+        if word in d:
+            print(' '.join(map(str, d[word])))
+        else:
+            print(-1)
