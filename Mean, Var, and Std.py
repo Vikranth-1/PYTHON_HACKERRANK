238f03b7107ef39e861e19fa1f88b2528ae3820c
Vikranth V <vvikranth08@gmail.com>
2026-05-22 00:24:06 +0530

    Create Mean, Var, and Std.py

---
+++ Mean, Var, and Std.py
@@ -0,0 +1,8 @@
+import numpy
+
+n, m = map(int, input().split())
+arr = numpy.array([list(map(int, input().split())) for _ in range(n)])
+
+print(numpy.mean(arr, axis=1))
+print(numpy.var(arr, axis=0))
+print(round(numpy.std(arr, axis=None), 11))
