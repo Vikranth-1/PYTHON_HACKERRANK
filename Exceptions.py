4b58c259b8e7fca0cc90c229930e81f5727252fa
Vikranth V <vvikranth08@gmail.com>
2026-05-28 07:13:33 +0530

    Create Exceptions.py

---
+++ Exceptions.py
@@ -0,0 +1,9 @@
+t = int(input())
+for _ in range(t):
+    try:
+        a, b = input().split()
+        print(int(a) // int(b))
+    except ZeroDivisionError as e:
+        print("Error Code: integer division or modulo by zero")
+    except ValueError as e:
+        print("Error Code:", e)
